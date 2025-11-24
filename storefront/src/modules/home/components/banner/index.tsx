eecb0917cee19fe7a46fa97755515c82e3845726
mikejianxu <mikejianxu@gmail.com>
2025-11-25 01:37:27 +0800

    add ShadcnUI components

--- storefront/src/modules/home/components/banner/index.tsx
+++ storefront/src/modules/home/components/banner/index.tsx
@@ -1,6 +1,6 @@
 import { BannerData } from 'types/cms'
 import Image from '@modules/common/components/cms/image'
-import { Button } from '@lib/components/ui/button'
+import { Button } from '@lib/components/ui'
 
 
 export default function Banner({ data }: { data: BannerData }) {
@@ -13,7 +13,7 @@ export default function Banner({ data }: { data: BannerData }) {
         height={600} />
       <h2>{title}</h2>
       <p>{text}</p>
-      <Button variant="default">{cta.text}</Button>
+      <Button>{cta.text}</Button>
     </div>
   )
 }
