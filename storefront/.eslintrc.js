e6bae86ed4b5c4a8815193644a77c15cff5f5fa2
mikejianxu <mikejianxu@gmail.com>
2025-11-26 02:01:23 +0800

    config prettier format config

--- storefront/.eslintrc.js
+++ storefront/.eslintrc.js
@@ -1,3 +1,2 @@
 module.exports = {
-  extends: ["next/core-web-vitals"]
 };
--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,42 +1,34 @@
-import { Suspense } from "react"
-
-import { listRegions } from "@lib/data/regions"
-import { StoreRegion } from "@medusajs/types"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import CartButton from "@modules/layout/components/cart-button"
-import SideMenu from "@modules/layout/components/side-menu"
-import Image from "next/image"
-import { ShoppingCartIcon, UserIcon } from "lucide-react"
+import Image from 'next/image'
+import { Suspense } from 'react'
+import { ShoppingCartIcon, UserIcon } from 'lucide-react'
+import { StoreRegion } from '@medusajs/types'
+import { listRegions } from '@lib/data/regions'
+import LocalizedClientLink from '@modules/common/components/localized-client-link'
+import CartButton from '@modules/layout/components/cart-button'
+import SideMenu from '@modules/layout/components/side-menu'
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
     <header className="sticky top-0 z-50 group h-16 mx-auto border-b duration-200">
-      <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+      <nav className="content-container flex items-center justify-between w-full h-full text-small-regular">
         <div className="flex-1 basis-0 h-full flex items-center">
-          <div className="h-full">
-            <SideMenu regions={regions} />
-          </div>
+          <SideMenu regions={regions} />
         </div>
 
         <div className="flex items-center h-full">
           <LocalizedClientLink
             href="/"
             className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-            data-testid="nav-store-link"
-          >
+            data-testid="nav-store-link">
             <Image src="/logo.svg" alt="Logo" width={100} height={100} />
           </LocalizedClientLink>
         </div>
 
         <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
           <div className="small:flex items-center gap-x-6 h-full">
-            <LocalizedClientLink
-              className="hover:text-ui-fg-base"
-              href="/account"
-              data-testid="nav-account-link"
-            >
+            <LocalizedClientLink className="hover:text-ui-fg-base" href="/account" data-testid="nav-account-link">
               <UserIcon />
             </LocalizedClientLink>
           </div>
@@ -45,8 +37,7 @@ export default async function Nav() {
               <LocalizedClientLink
                 className="hover:text-ui-fg-base flex gap-2"
                 href="/cart"
-                data-testid="nav-cart-link"
-              >
+                data-testid="nav-cart-link">
                 <ShoppingCartIcon />
               </LocalizedClientLink>
             }>
