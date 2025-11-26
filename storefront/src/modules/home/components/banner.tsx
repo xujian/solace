4ced8f4b696ca1125ec34a51f91916fa08d8b998
mikejianxu <mikejianxu@gmail.com>
2025-11-26 19:15:45 +0800

    +  prettier-plugin-tailwindcss auto sort tailwind classes

--- storefront/src/modules/home/components/banner.tsx
+++ storefront/src/modules/home/components/banner.tsx
@@ -1,16 +1,12 @@
 import { BannerData } from 'types/cms'
-import Image from '@modules/common/components/cms/image'
 import { Button } from '@lib/components/ui'
-
+import Image from '@modules/common/components/cms/image'
 
 export default function Banner({ data }: { data: BannerData }) {
   const { title, text, cta, image } = data
   return (
-    <div className='banner w-full h-[75vh]'>
-      <Image src={image.url}
-        alt={image.alternativeText || ''}
-        width={1000}
-        height={600} />
+    <div className="banner h-[75vh] w-full">
+      <Image src={image.url} alt={image.alternativeText || ''} width={1000} height={600} />
       <h2>{title}</h2>
       <p>{text}</p>
       <Button>{cta.text}</Button>
