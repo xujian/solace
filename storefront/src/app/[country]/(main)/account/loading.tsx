3151bb5783705f88f4035a19892fc4fab3972742
mikejianxu <mikejianxu@gmail.com>
2025-12-18 01:50:37 +0800

    remove redundant loading.tsx

--- storefront/src/app/[country]/(main)/account/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { RefreshCw as Spinner } from 'lucide-react'
-
-export default function Loading() {
-  return (
-    <div className="text-ui-fg-base flex h-full w-full items-center justify-center">
-      <Spinner size={36} />
-    </div>
-  )
-}
